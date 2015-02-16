5a197f772fa4afe1f0bb895f81cb8fca60780823
toyama-h <toyama-h@ipros.jp>
2015-02-16 19:42:06 +0900

    add before and after callback

--- lib/capistrano/erb_sudo_upload/task.rb
+++ lib/capistrano/erb_sudo_upload/task.rb
@@ -10,23 +10,52 @@ module Capistrano
           yaml = YAML.load(ERB.new(File.read(config_path)).result(binding))
           set_vars(yaml['vars'])
           yaml['settings'].each do |key, setting|
-            role_map = Util.get_role_map(setting)
+            before_commands = setting['before_commands']
+            after_commands = setting['after_commands']
+            file_settings = setting['file_settings']
+            role_map = Util.get_role_map(file_settings)
 
             method_names = []
             namespace key do
+              desc "deploy #{key}"
               task "default", except: {no_release: true} do
                 puts "#{key} deploy start"
               end
               role_map.each do |role_name, files|
                 method_names << role_name
                 option = role_name == 'all' ? { except: {no_release: true} } : { roles: role_name, except: {no_release: true} }
+                desc "deploy [#{files.join(', ')}], Role is #{role_name} only"
                 task(role_name, option) do
-                  sudo_upload_with_files(key, files, setting)
+                  sudo_upload_with_files(key, files, file_settings)
+                end
+              end
+
+              exec_commands_option = role_map.keys.include?('all') ? {} : { roles: role_map.keys }
+              if before_commands
+                desc "exec commands => #{before_commands.join(';')}"
+                task('before', exec_commands_option) do
+                  before_commands.each do |before_command|
+                    run "#{sudo} #{before_command}" unless fetch(:erb_sudo_upload_dryrun, false)
+                  end
+                end
+              end
+
+              if after_commands
+                desc "exec commands => #{after_commands.join(';')}"
+                task('after', exec_commands_option) do
+                  after_commands.each do |after_command|
+                    run "#{sudo} #{after_command}" unless fetch(:erb_sudo_upload_dryrun, false)
+                  end
                 end
               end
             end
-            task_names = method_names.map{ |role_name| "erb_sudo_upload:#{key}:#{role_name}"}
-            after *(["erb_sudo_upload:#{key}"].concat(task_names))
+            deploy_task_names = method_names.map{ |role_name| "erb_sudo_upload:#{key}:#{role_name}"}
+
+            tasks = ["erb_sudo_upload:#{key}"]
+            tasks << "erb_sudo_upload:#{key}:before" if before_commands
+            tasks = tasks.concat(deploy_task_names)
+            tasks << "erb_sudo_upload:#{key}:after" if after_commands
+            after *tasks
           end
         end
       end
