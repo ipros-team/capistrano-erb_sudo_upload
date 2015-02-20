2f0a7c3abd5aafac0bd61a799760648145f8a62e
toyama-h <toyama-h@ipros.jp>
2015-02-20 20:20:10 +0900

    at dryrun, output command

--- lib/capistrano/erb_sudo_upload/core.rb
+++ lib/capistrano/erb_sudo_upload/core.rb
@@ -7,7 +7,11 @@ module Capistrano::ErbSudoUpload
         def self.run_sudo_commands(commands)
           switchuser(fetch(:erb_sudo_upload_user, fetch(:user))) do
             commands.each do |command|
-              run "#{sudo} #{command}" unless fetch(:erb_sudo_upload_dryrun, false)
+              unless fetch(:erb_sudo_upload_dryrun, false)
+                run "#{sudo} #{command}"
+              else
+                puts "#{sudo} #{command}"
+              end
             end
           end
         end
